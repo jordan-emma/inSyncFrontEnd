d1448f35766a77548788bd286aea91cfb50f9666
Jordan Allen <jordan.allen06@outlook.com>
2024-08-13 17:56:51 +1000

    Play page created

---
+++ app/src/views/PlayView.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="container">
+    <div class="back">
+      <button>Back</button>
+    </div>
+    <div class="underlineButtonContainer">
+      <button class="underlineButton">Join Game</button>
+      <button class="underlineButton">Host Game</button>
+    </div>
+      <div class="form-group">
+        <label for="roomCode">Room code:</label>
+        <input id="roomCode" type="text"/>
+      </div>
+      <div class="form-group">
+        <label for="yourName">Your name:</label>
+        <input id="yourName" type="text"/>
+      </div>
+    <div class="button-container">
+      <button class="rounded-button">JOIN GAME</button>
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+  background-image: url('../images/playBackground9.webp'); 
+  background-size: cover; 
+}
+
+  .form-group {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-bottom: 1rem;
+  width: 100%;
+  max-width: 300px;
+  background-color: rgba(255, 255, 255, 0.3);
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+}
+
+.form-group label {
+  color: #000;
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+}
+
+.form-group input {
+  width: 100%;
+  box-sizing: border-box;
+  font-size: 1rem;
+  border: 1px solid #ddd; 
+  border-radius: 0.5rem; 
+  padding: 0.5rem; 
+  outline: none; 
+}
+
+.back {
+  position: absolute; 
+  top: 1rem; 
+  left: 1rem; 
+  margin: 0; 
+}
+
+.back button {
+  background: transparent; 
+  border: none; 
+  padding: 0.625rem 1.25rem; 
+  font-size: 1.2rem; 
+  font-weight: 800; 
+  color: black; 
+  text-decoration: underline; 
+  text-decoration-thickness: 2px; 
+  border-radius: 1.25rem; 
+  cursor: pointer; 
+  transition: background-color 0.3s ease, color 0.3s ease; 
+  outline: none; 
+}
+
+.back button:hover {
+  background-color: black; 
+  color: white; 
+}
+
+.underlineButton{ 
+  background: transparent; 
+  border: none; 
+  padding-bottom: 1rem;
+  padding-top: 0.5rem; 
+  font-size: 1.5rem; 
+  font-weight: 600; 
+  color: black; 
+  text-decoration: underline;
+  text-decoration-thickness: 4px; 
+  text-align: center;
+  cursor: pointer; 
+  transition: background-color 0.3s ease, color 0.3s ease; 
+  opacity: 1; 
+  width: 10rem; 
+  outline: none; 
+  border-radius: 1.25rem;
+}
+.underlineButtonContainer{
+  gap: 0.5em; 
+}
+
+.underlineButton:hover {
+  background-color: black; 
+  color: white; 
+}
+</style>
