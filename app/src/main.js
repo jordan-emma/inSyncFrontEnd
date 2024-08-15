48bbb90b32424ff988c2cbd38cc229a95e84315b
HaydenNorris <92356325+HaydenNorris@users.noreply.github.com>
2024-08-15 10:43:51 +1000

    Connect to backend
    * set up global stores for user and game
    * working requests made to backend using tokens

--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,23 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router'
-
+import axios from 'axios'
 const app = createApp(App)
+import { userStore } from '@/stores/user.js'
+import { gameStore } from '@/stores/game.js'
 
-app.use(router)
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.withCredentials = true;
 
-app.mount('#app')
+app.use(router);
+app.use(createPinia());
+
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$userStore = userStore();
+app.config.globalProperties.$gameStore = gameStore();
+
+
+app.mount('#app');
