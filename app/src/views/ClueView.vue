3e7109ca04e1e124c971e16da82cf0737b557c55
Jordan Allen <jordan.allen06@outlook.com>
2024-08-16 20:57:47 +1000

    Page heading added

--- app/src/views/ClueView.vue
+++ app/src/views/ClueView.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="purpleBackground">
-    
+    <div class="container">
+      <div class="back">
+        <button @click="toggleBack">Back</button>
+      </div>
+      <div class="pageHeading">
+        <h2>Type a Clue</h2>
+      </div>
+    </div>
   </div>
-
 </template>
 
 <script>
