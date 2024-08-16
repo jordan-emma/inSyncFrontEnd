8ee10d00ffaf18256de377a759ac1c6d38187b80
Jordan Allen <jordan.allen06@outlook.com>
2024-08-16 21:13:51 +1000

    clue page fields added

--- app/src/views/ClueView.vue
+++ app/src/views/ClueView.vue
@@ -7,15 +7,42 @@
       <div class="pageHeading">
         <h2>Type a Clue</h2>
       </div>
+      <div>
+        <p>Place holder for slider</p>
+      </div>
+      <div class="form-group">
+        <input id="clueField" type="text" v-model="clue"/>
+    </div>
+    <div class="button-container">
+      <button class="rounded-button">New Scale</button>
+      <button class="rounded-button">Submit</button>
+    </div>
+    <div>
+      <h2>1/3</h2>
+    </div>
     </div>
   </div>
 </template>
 
 <script>
-
+  export default {
+    data() {
+      return {
+        clue: 'Your clue...',
+      };
+    }
+  }
 </script>
 
 <style scoped>
 
+  .button-container{
+    gap: 1rem; 
+    display: flex; 
+  }
+
+  h2{
+    color: white;
+  }
 
 </style>
