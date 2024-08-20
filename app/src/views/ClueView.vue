2a5f3b95218aca9bce747d4f9d2772e7e0f9c219
haydennorris <hayden@mxstore.com.au>
2024-08-21 07:16:05 +1000

    tidy up and fetch clue on next and back clicks

--- app/src/views/ClueView.vue
+++ app/src/views/ClueView.vue
@@ -7,13 +7,13 @@
       <div class="pageHeading">
         <h2>Type a Clue</h2>
       </div>
-      <div v-if = "gotClue" class="slider-wrapper">
+      <div v-if="gotClue" class="slider-wrapper">
         <p class="clueScalePhrase1">{{ clueObject.high }}</p>
-        <Slider :min="0" :max="clueObject.max_value" :value="clueObject.value" :disabled="disableSlider" /> 
+        <Slider :min="0" :max="clueObject.max_value" :value="clueObject.value" :disabled="true" />
         <p class="clueScalePhrase2">{{ clueObject.low }}</p>
       </div>
       <div class="form-group">
-        <input id="clueField" type="text" v-model="currentClue" placeholder='Enter your clue...'/>
+        <input id="clueField" type="text" v-model="currentClue" placeholder='Enter your clue...' />
       </div>
       <div class="button-container">
         <button v-if="clueNumber > 1" class="rounded-button" @click="goToPreviousClue">Back</button>
@@ -22,65 +22,61 @@
         <button v-if="clueNumber === maxClues" class="rounded-button" @click="goToGuess">Submit</button>
       </div>
       <div>
-        <h2>{{clueNumber}}/3</h2>
+        <h2>{{ clueNumber }}/3</h2>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Slider from '../components/slider.vue'; 
+import Slider from '../components/slider.vue'
 
 export default {
-  created(){
-      this.getClue(); 
-    },
+  created() {
+    this.getClue()
+  },
   components: {
     Slider
   },
   data() {
     return {
-      clues: [],
-      currentClue: '', 
+      currentClue: '',
       clueNumber: 1,
-      disableSlider: true,
       clueObject: {},
-      gotClue: false, 
-      maxClues: 3,
-    };
+      gotClue: false,
+      maxClues: 3
+    }
   },
   methods: {
     addClue() {
-      if (this.currentClue.trim()){
-        this.clues.push(this.currentClue);
-        this.currentClue ='';
+      if (this.currentClue.trim()) {
+        this.currentClue = ''
       }
-      console.log(this.clues);
     },
-    async getClue(){
-    let response = await this.$axios.get(`/game/${this.$gameStore.game.id}/clue/${this.clueNumber}`)
-    if (response.status !== 200){
-      throw 'Failed to get clue'
-      this.gotClue = false; 
-    }
-     this.clueObject = response.data; 
-     this.gotClue = true; 
-    }, 
+    async getClue() {
+      let response = await this.$axios.get(`/game/${this.$gameStore.game.id}/clue/${this.clueNumber}`)
+      if (response.status !== 200) {
+        throw 'Failed to get clue'
+        this.gotClue = false
+      }
+      this.clueObject = response.data
+      this.gotClue = true
+    },
     changeClueNumber() {
-      this.clueNumber++;
-      this.addClue();
-      this.getClue(); 
+      this.clueNumber++
+      this.addClue()
+      this.getClue()
     },
     toggleBack() {
-      this.$router.push('/lobby');
-    }, 
+      this.$router.push('/lobby')
+    },
     goToPreviousClue() {
-      this.clueNumber--; 
-      this.clues.pop(); 
+      this.clueNumber--
+      this.getClue()
     },
 
-    goToGuess(){ 
-      this.$router.push('/guess');
+    goToGuess() {
+      this.$router.push('/guess')
     }
   }
 }
@@ -89,20 +85,19 @@ export default {
 
 <style scoped>
 
-  .button-container{
-    gap: 1rem; 
-    display: flex; 
-  }
-
-  h2, p{
-    color: white;
-  }
-
 .button-container {
   gap: 1rem;
   display: flex;
 }
 
+h2, p {
+  color: white;
+}
+
+.button-container {
+  gap: 1rem;
+  display: flex;
+}
 
 
 </style>
--- app/src/views/PlayView.vue
+++ app/src/views/PlayView.vue
@@ -1,112 +1,111 @@
 <template>
   <div class="purpleBackground">
-  <div class="container">
-    <div class="back">
-      <button @click="toggleBack">Back</button>
+    <div class="container">
+      <div class="back">
+        <button @click="toggleBack">Back</button>
+      </div>
+      <div class="underlineButtonContainer">
+        <button v-if="showRoomCodeField" class="currentPageButton" @click="toggleJoinGame">Join Game</button>
+        <button v-if="!showRoomCodeField" class="underlineButton" @click="toggleJoinGame">Join Game</button>
+        <button v-if="showRoomCodeField" class="underlineButton" @click="toggleHostGame">Host Game</button>
+        <button v-if="!showRoomCodeField" class="currentPageButton" @click="toggleHostGame">Host Game</button>
+      </div>
+      <div v-if="showRoomCodeField" class="form-group">
+        <label for="roomCode">Room code:</label>
+        <input id="roomCode" type="text" v-model="code" />
+      </div>
+      <div class="form-group">
+        <label for="yourName">Your name:</label>
+        <input id="yourName" type="text" v-model="name" />
+      </div>
+      <div class="button-container">
+        <button class="rounded-button" @click="startGame">Let's Play</button>
+      </div>
     </div>
-    <div class="underlineButtonContainer">
-      <button v-if="showRoomCodeField" class="currentPageButton" @click="toggleJoinGame">Join Game</button>
-      <button v-if="!showRoomCodeField" class="underlineButton" @click="toggleJoinGame">Join Game</button>
-      <button v-if="showRoomCodeField" class="underlineButton" @click="toggleHostGame">Host Game</button>
-      <button v-if="!showRoomCodeField" class="currentPageButton" @click="toggleHostGame">Host Game</button>
-    </div>
-    <div v-if="showRoomCodeField" class="form-group">
-      <label for="roomCode">Room code:</label>
-      <input id="roomCode" type="text" v-model="code"/>
-    </div>
-    <div class="form-group">
-      <label for="yourName">Your name:</label>
-      <input id="yourName" type="text" v-model="name" />
-    </div>
-    <div class="button-container">
-      <button class="rounded-button" @click="startGame">Let's Play</button>
-    </div>
-  </div>
   </div>
 </template>
 
 
 <script>
 export default {
-  created(){
-    if(this.name === ''){
-      this.name = this.$userStore.name;
+  created() {
+    if (this.name === '') {
+      this.name = this.$userStore.name
     }
   },
   data() {
     return {
       showRoomCodeField: true,
       name: '',
-      code: '',
+      code: ''
     }
   },
   methods: {
     toggleHostGame() {
-      this.showRoomCodeField = false;
+      this.showRoomCodeField = false
     },
     toggleJoinGame() {
-      this.showRoomCodeField = true;
-    }, 
+      this.showRoomCodeField = true
+    },
     toggleBack() {
-      this.$router.push('/landing');
+      this.$router.push('/landing')
     },
     async startGame() {
-      if(this.showRoomCodeField) {
-        await this.joinGame();
+      if (this.showRoomCodeField) {
+        await this.joinGame()
       } else {
-        await this.createGame();
+        await this.createGame()
       }
     },
     async createGame() {
-      try{
-        let code = await this.$gameStore.hostGame(this.name);
-        this.$router.push('/lobby');
-      } 
-      catch(e){
-          console.log(e);
-          alert('Failed to make game'); 
-        }
-      }, 
-      async joinGame() {
-        if(this.code.length !== 6){
-          console.log("Invalid code");
-          return;
-        }
-        let code = await this.$gameStore.joinGame(this.name, this.code);
-        this.$router.push('/lobby');
-      }, 
+      try {
+        let code = await this.$gameStore.hostGame(this.name)
+        this.$router.push('/lobby')
+      } catch (e) {
+        console.log(e)
+        alert('Failed to make game')
+      }
     },
+    async joinGame() {
+      if (this.code.length !== 6) {
+        console.log('Invalid code')
+        return
+      }
+      let code = await this.$gameStore.joinGame(this.name, this.code)
+      this.$router.push('/lobby')
+    }
+  }
 
 }
 </script>
 
 <style scoped>
 
-.underlineButtonContainer{
-  gap: 0.5em; 
+.underlineButtonContainer {
+  gap: 0.5em;
 }
 
 .underlineButton:hover {
-  background-color: black; 
-  color: white; 
+  background-color: black;
+  color: white;
 }
 
 .currentPageButton {
-  background-color: black; 
-  color: white; 
-  border: none; 
+  background-color: black;
+  color: white;
+  border: none;
   padding-bottom: 1rem;
-  padding-top: 0.5rem; 
-  font-size: 1.5rem; 
-  font-weight: 600; 
+  padding-top: 0.5rem;
+  font-size: 1.5rem;
+  font-weight: 600;
   text-decoration: underline;
-  text-decoration-thickness: 4px; 
+  text-decoration-thickness: 4px;
   text-align: center;
-  cursor: pointer; 
-  transition: background-color 0.3s ease, color 0.3s ease; 
-  opacity: 1; 
-  width: 10rem; 
-  outline: none; 
+  cursor: pointer;
+  transition: background-color 0.3s ease, color 0.3s ease;
+  opacity: 1;
+  width: 10rem;
+  outline: none;
   border-radius: 1.25rem;
 }
 
