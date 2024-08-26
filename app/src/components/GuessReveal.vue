500632baaf463773e9c96c5b9c4432846560c7b9
Jordan Allen <jordan.allen06@outlook.com>
2024-08-26 18:33:45 +1000

    guess reveal redesigned and made responsive in mobile

--- app/src/components/GuessReveal.vue
+++ app/src/components/GuessReveal.vue
@@ -2,17 +2,20 @@
   <div>
     <div class="pageHeading">
       <h2>Let's see how you did...</h2>
-      <h2>Clue: {{ prompt }}</h2>
     </div>
-    <div class="slider-wrapper">
-      <p>{{ clueObject.low }}</p>
-      <Slider :max="clueObject.max_value" :value="clueObject.guess_value" :disabled="true" />
-      <p>{{ clueObject.high }}</p>
-    </div>
-    <div class="slider-wrapper">
-      <p>{{ clueObject.low }}</p>
-      <Slider :max="clueObject.max_value" :value="clueObject.value" :disabled="true" />
-      <p>{{ clueObject.high }}</p>
+    <div>
+      <p class="prompt">{{ clueObject.player_name }}'s Clue: {{ prompt }}</p>
+      <div class="slider-wrapper">
+        <p>{{ clueObject.low }}</p>
+        <Slider :max="clueObject.max_value" :value="clueObject.value" :disabled="true" />
+        <p>{{ clueObject.high }}</p>
+      </div>
+      <p class="prompt">Your guess:</p>
+      <div class="slider-wrapper">
+        <p>{{ clueObject.low }}</p>
+        <Slider :max="clueObject.max_value" :value="clueObject.guess_value" :disabled="true" />
+        <p>{{ clueObject.high }}</p>
+      </div>
     </div>
     <div class="button-container">
       <button v-if='host && !isGameFinished' class="rounded-button" @click="goToNextClue">Next</button>
@@ -79,5 +82,47 @@ export default {
 h2, p {
   color: white;
 }
+
+.prompt { 
+  font-size: 2em;
+  justify-content: center;
+  display: flex;
+}
+.pageHeading { 
+  margin-bottom: 2em;
+}
+
+@media (max-width: 600px) {
+  .pageHeading  {
+    margin-bottom: auto;
+    margin-top: 2em;
+  }
+  .slider-wrapper{
+    display: flex;
+    width: auto; 
+    margin-bottom: 1em;
+    padding-bottom: 0;
+  }
+}
+
+@media (max-width: 1024px) and (max-height: 600px) {
+  .pageHeading  {
+    margin-bottom: auto;
+    margin-top: 2em;
+  }
+
+  .slider-wrapper {
+    display: flex;
+    justify-content: center; 
+    align-items: center;     
+    width: 100%;             
+    margin: 0 auto;          
+    padding-bottom: 0;
+  }
+  .button-container {
+  padding-bottom: 1em;
+}
+}
+
 </style>
 
