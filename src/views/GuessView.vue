9aae2f6c599d454350f4f9db59082d2e1b905a15
Jordan Allen <jordan.allen06@outlook.com>
2024-08-29 18:43:22 +1000

    Guess View reformatted with prettier

--- src/views/GuessView.vue
+++ src/views/GuessView.vue
@@ -41,88 +41,85 @@ export default {
       clueGiverId: 0,
       maxValue: 0,
       clueId: this.$gameStore.game?.current_clue_id,
-      clueObject: {},
-    };
+      clueObject: {}
+    }
   },
   watch: {
     $gameStore: {
       deep: true,
       handler() {
-        this.clueId = this.$gameStore.game.current_clue_id;
-      },
+        this.clueId = this.$gameStore.game.current_clue_id
+      }
     },
     $clueStore: {
       deep: true,
       handler() {
-        this.setClueProperties();
-      },
+        this.setClueProperties()
+      }
     },
     clueId() {
-      this.setClueProperties();
-    },
+      this.setClueProperties()
+    }
   },
   async created() {
-    if(!this.$userStore.isLoggedIn && this.$gameStore.empty){ 
-      return 
+    if (!this.$userStore.isLoggedIn && this.$gameStore.empty) {
+      return
     }
-    this.setClueProperties();
+    this.setClueProperties()
   },
   computed: {
     clueGiverName() {
-      return this.isClueGiver ? 'Your' : `${this.clueGiver}'s`;
+      return this.isClueGiver ? 'Your' : `${this.clueGiver}'s`
     },
-    isClueGiver() { 
-      return this.$userStore.user.id === this.clueGiverId; 
+    isClueGiver() {
+      return this.$userStore.user.id === this.clueGiverId
     },
     isHost() {
-      return this.$gameStore.hostPlayerId === this.$userStore.id;
+      return this.$gameStore.hostPlayerId === this.$userStore.id
     }
   },
   methods: {
     toggleBack() {
-      this.$router.push('/play');
-    }, 
+      this.$router.push('/play')
+    },
     changeClueNumber() {
-      if(this.clueNumber < this.totalCluesProvided){
-        this.clueNumber++;
+      if (this.clueNumber < this.totalCluesProvided) {
+        this.clueNumber++
       }
-    }, 
+    },
     async setCurrentClue() {
-      if (!this.isHost){
-        return;
+      if (!this.isHost) {
+        return
       }
 
       if (!this.clueId) {
         try {
-          await this.$gameStore.setNextGuessId();
+          await this.$gameStore.setNextGuessId()
         } catch (e) {
-          console.log(e);
+          console.log(e)
         }
       }
     },
     setClueProperties() {
-      if(!this.clueId){
-        this.setCurrentClue();
-        return;
-      }
-      let data = this.$clueStore.clue_by_id(this.clueId);
-      if (!data) {
-        this.$clueStore.fetchClue(this.clueId);
+      if (!this.clueId) {
+        this.setCurrentClue()
         return
       }
-      this.clueObject = data;
-      this.prompt = data.prompt;
-      this.clueGiver = data.player_name;
-      this.totalCluesProvided = data.total_clues;
-      this.clueGiverId = data.player_id;
-      this.clueId = data.id;
+      let data = this.$clueStore.clue_by_id(this.clueId)
+      if (!data) {
+        this.$clueStore.fetchClue(this.clueId)
+        return
+      }
+      this.clueObject = data
+      this.prompt = data.prompt
+      this.clueGiver = data.player_name
+      this.totalCluesProvided = data.total_clues
+      this.clueGiverId = data.player_id
+      this.clueId = data.id
     },
-    capitalizeString(string) { 
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    },
-
+    capitalizeString(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1)
+    }
   }
 }
 </script>
-
-
