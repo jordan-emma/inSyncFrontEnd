ab9532fa43b09392acb59f3d7460c1b2bda686f5
Jordan Allen <jordan.allen06@outlook.com>
2024-08-29 19:57:31 +1000

    Fun facts modal reformatted with prettier

--- src/components/funFactsModal.vue
+++ src/components/funFactsModal.vue
@@ -3,16 +3,19 @@
     <div class="content" @click.stop>
       <div class="header">
         <div class="closeButton">
-          <img src="../images/close.png" @click="exit" alt="Close"/>
+          <img src="../images/close.png" @click="exit" alt="Close" />
         </div>
-        <img class="waitingIcon" src="../images/waitingIcon.png" alt="Waiting Icon"/>
+        <img class="waitingIcon" src="../images/waitingIcon.png" alt="Waiting Icon" />
         <h2>Waiting for other players...</h2>
       </div>
       <p class="objective" v-html="header"></p>
       <div class="carousel">
         <transition name="fade" mode="out-in">
           <div class="whiteBox" :key="currentIndex">
-            <p><b>{{ shuffledBlocks[currentIndex].title }}: </b>{{ shuffledBlocks[currentIndex].body }}</p>
+            <p>
+              <b>{{ shuffledBlocks[currentIndex].title }}: </b
+              >{{ shuffledBlocks[currentIndex].body }}
+            </p>
           </div>
         </transition>
         <div class="button-container">
@@ -44,66 +47,64 @@ export default {
     return {
       currentIndex: 0,
       shuffledBlocks: this.shuffleBlocks(this.blocks)
-    };
+    }
   },
   methods: {
     close(e) {
-      if (e.target.className === "container") {
-        this.exit();
+      if (e.target.className === 'container') {
+        this.exit()
       }
     },
     exit() {
-      this.$emit('close');
+      this.$emit('close')
     },
     nextSlide() {
       if (this.currentIndex < this.shuffledBlocks.length - 1) {
-        this.currentIndex++;
+        this.currentIndex++
       } else {
-        this.currentIndex = 0;
+        this.currentIndex = 0
       }
     },
     prevSlide() {
       if (this.currentIndex > 0) {
-        this.currentIndex--;
+        this.currentIndex--
       } else {
-        this.currentIndex = this.shuffledBlocks.length - 1;
+        this.currentIndex = this.shuffledBlocks.length - 1
       }
     },
     shuffleBlocks(blocks) {
-      let shuffled = blocks.slice(); 
+      let shuffled = blocks.slice()
       for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; 
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
       }
-      return shuffled;
+      return shuffled
     }
   }
-};
+}
 </script>
 
-
 <style scoped>
-
 .container {
-  height: 100vh; 
+  height: 100vh;
   width: 100vw;
   z-index: 100;
   background-color: rgba(94, 94, 94, 0.5);
-  position: absolute; 
+  position: absolute;
 }
 
 .content {
-  height: auto; 
-  max-width: 700px; 
+  height: auto;
+  max-width: 700px;
   width: 70vw;
   background-color: white;
   border-radius: 1rem;
   box-shadow: -10px 10px 20px 5px rgba(29, 22, 51, 0.6);
   font-family: 'Arial', 'sans-serif';
-  font-size: 0.98rem; 
+  font-size: 0.98rem;
   overflow-y: auto;
-  padding-bottom: 2em; 
-  background-color: #60129d; 
+  padding-bottom: 2em;
+  background-color: #60129d;
 }
 
 .header {
@@ -116,8 +117,8 @@ export default {
 }
 
 .header img {
-  max-width: 50px; 
-  margin-right: 1em; 
+  max-width: 50px;
+  margin-right: 1em;
   margin-left: 1em;
 }
 
@@ -131,17 +132,17 @@ h2 {
 }
 
 .closeButton {
-  position: absolute; 
-  top: 1.2em; 
-  right: 0.1em; 
+  position: absolute;
+  top: 1.2em;
+  right: 0.1em;
   margin: 0;
   background-color: transparent;
   border: transparent;
 }
 
 .closeButton img {
-  width: 30px; 
-  height: 30px; 
+  width: 30px;
+  height: 30px;
 }
 
 @media (max-width: 600px) {
@@ -154,7 +155,7 @@ h2 {
     font-size: 0.95em;
   }
   .whiteBox {
-    padding: 10px; 
+    padding: 10px;
   }
   .objective {
     font-size: 1.2em;
@@ -166,67 +167,69 @@ h2 {
   background-color: white;
   border-radius: 4px;
   width: 70%;
-  height: auto; 
+  height: auto;
   display: flex;
   flex-direction: column;
   text-align: center;
   align-items: center;
   justify-content: center;
-  margin: 10px auto; 
-  padding: 20px; 
+  margin: 10px auto;
+  padding: 20px;
   color: #241451;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); 
-  transition: 0.5s ease-in-out; 
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  transition: 0.5s ease-in-out;
 }
 
 .carousel {
   position: relative;
   width: 100%;
   overflow: hidden;
-  text-align: center; 
+  text-align: center;
 }
 
-.objective { 
-  color: white; 
+.objective {
+  color: white;
   font-size: 1.5em;
   text-align: center;
-  line-height: 1.2em; 
+  line-height: 1.2em;
 }
 
 .button-container {
-  margin-top: 10px; 
+  margin-top: 10px;
   display: flex;
-  justify-content: center; 
-  width: 100%; 
+  justify-content: center;
+  width: 100%;
 }
 
-.prev, .next {
+.prev,
+.next {
   background-color: rgba(0, 0, 0, 0.5);
   color: white;
   border: none;
   padding-top: 0.5rem;
-  padding-bottom: 0.5rem; 
-  padding-left: 1rem; 
-  padding-right: 1rem; 
+  padding-bottom: 0.5rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
   cursor: pointer;
   font-size: 2em;
-  border-radius:0.5rem;
+  border-radius: 0.5rem;
   margin: 5px;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.4); 
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.4);
 }
 
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 0.7s ease;
 }
 
-.fade-enter, .fade-leave-to {
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
 }
 
-.waitingIcon{ 
- height: 3em; 
- width: 3em;
- margin: 0.4em;
+.waitingIcon {
+  height: 3em;
+  width: 3em;
+  margin: 0.4em;
 }
-
 </style>
