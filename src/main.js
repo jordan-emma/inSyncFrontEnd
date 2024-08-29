baabedf46a4f4ce83067825e8445d04939ca6e7f
Jordan Allen <jordan.allen06@outlook.com>
2024-08-29 15:35:16 +1000

    Reformatted main.js with Prettier

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
 import './assets/main.css'
 import { infoToast, successToast, errorToast, Toast, goodRequest, badRequest } from './js/toast.js'
 import { socket, connectSocket } from './js/socket.js'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPersist from 'pinia-plugin-persist'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import { userStore } from '@/stores/user.js'
+import { gameStore } from '@/stores/game.js'
+import { loadingStore } from './stores/loading'
+import { clueStore } from './stores/clue.js'
 
 const pinia = createPinia()
 pinia.use(piniaPersist)
 
 const app = createApp(App)
-app.use(pinia)
-
-import { userStore } from '@/stores/user.js'
-import { gameStore } from '@/stores/game.js'
-import { loadingStore } from './stores/loading'
-import { clueStore } from './stores/clue.js'
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 axios.defaults.withCredentials = true
@@ -36,6 +33,7 @@ app.config.globalProperties.$badRequest = badRequest
 app.config.globalProperties.$socket = socket
 app.config.globalProperties.$connectSocket = connectSocket
 
+app.use(pinia)
 app.use(router)
 app.use(Toast, {})
 app.mount('#app')
