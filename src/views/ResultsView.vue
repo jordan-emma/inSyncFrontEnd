17fb426d0c9ea861a35ccfc407ba0d4529306b2b
Jordan Allen <jordan.allen06@outlook.com>
2024-08-29 19:47:17 +1000

    Results view styling tags removed, reformatted with prettier

--- src/views/ResultsView.vue
+++ src/views/ResultsView.vue
@@ -1,61 +1,55 @@
 <template>
   <div class="purpleBackground">
     <div class="pageContainer">
-     <ResultsModal 
-     :show="true"
-     :header="modalTitle"
-     :blocks="modalBlocks"
-     :modalHeading="modalHeading" 
-     />   
+      <ResultsModal
+        :show="true"
+        :header="modalTitle"
+        :blocks="modalBlocks"
+        :modalHeading="modalHeading"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import ResultsModal from '@/components/ResultsModal.vue';
+import ResultsModal from '@/components/ResultsModal.vue'
 
 export default {
   components: {
-    ResultsModal, 
+    ResultsModal
   },
   data() {
     return {
       modalTitle: 'Total Points: ',
       modalHeading: 'Results',
-      modalBlocks: [],
+      modalBlocks: []
     }
   },
-  methods: { 
-    setBlocks() { 
-      let game_id=this.$gameStore.id;
-      let clues=this.$clueStore.getCluesForGame(game_id);
-      return clues.map((clue) => { 
+  methods: {
+    setBlocks() {
+      let game_id = this.$gameStore.id
+      let clues = this.$clueStore.getCluesForGame(game_id)
+      return clues.map((clue) => {
         return {
-          title: 'Clue', 
-          body: clue.prompt, 
-          max_value: clue.max_value, 
-          low_value: clue.low, 
+          title: 'Clue',
+          body: clue.prompt,
+          max_value: clue.max_value,
+          low_value: clue.low,
           guess_value: clue.guess_value,
-          target_value: clue.value, 
-          low_prompt: clue.low, 
+          target_value: clue.value,
+          low_prompt: clue.low,
           high_prompt: clue.high,
           score: clue.score,
-          player_name: clue.player_name,
+          player_name: clue.player_name
         }
       })
-    }, 
-
-  }, 
-  created() { 
-    if(!this.$userStore.isLoggedIn && this.$gameStore.empty){
+    }
+  },
+  created() {
+    if (!this.$userStore.isLoggedIn && this.$gameStore.empty) {
       return
     }
-    this.modalBlocks = this.setBlocks();
-  },
+    this.modalBlocks = this.setBlocks()
+  }
 }
-
 </script>
-
-<style scoped>
-
-</style>
