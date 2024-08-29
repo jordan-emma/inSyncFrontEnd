28c8b6bdeb375b8fac5764ab326b33013de63433
Jordan Allen <jordan.allen06@outlook.com>
2024-08-29 15:46:02 +1000

    main js restructured for pinia

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import './assets/main.css'
 import { infoToast, successToast, errorToast, Toast, goodRequest, badRequest } from './js/toast.js'
 import { socket, connectSocket } from './js/socket.js'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPersist from 'pinia-plugin-persist'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+
 import { userStore } from '@/stores/user.js'
 import { gameStore } from '@/stores/game.js'
 import { loadingStore } from './stores/loading'
@@ -16,6 +18,7 @@ const pinia = createPinia()
 pinia.use(piniaPersist)
 
 const app = createApp(App)
+app.use(pinia)
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 axios.defaults.withCredentials = true
@@ -33,7 +36,6 @@ app.config.globalProperties.$badRequest = badRequest
 app.config.globalProperties.$socket = socket
 app.config.globalProperties.$connectSocket = connectSocket
 
-app.use(pinia)
 app.use(router)
 app.use(Toast, {})
 app.mount('#app')
