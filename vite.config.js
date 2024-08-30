ad26cb0ea164ef253c72d74b1e51ef3388e605e8
Jordan Allen <jordan.allen06@outlook.com>
2024-08-30 13:22:22 +1000

    Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/inSyncFrontEnd/',  // Set this to your repository name
   plugins: [
     vue(),
     vueJsx(),
