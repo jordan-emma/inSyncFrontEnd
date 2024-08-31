a28fc66d0a496b4a9c90c6fbc15d2bb9142468e2
Jordan Allen <jordan.allen06@outlook.com>
2024-08-31 15:57:28 +1000

    Button size on play view updated for mobile compatibility

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -118,7 +118,7 @@ export default {
   color: white;
   border: none;
   padding: 0.9rem 0;
-  font-size: 1.5rem;
+  font-size: 1.3rem;
   font-weight: 600;
   text-align: center;
   cursor: pointer;
@@ -129,5 +129,8 @@ export default {
   border-radius: 1rem;
   margin-bottom: 1rem;
   outline: none;
+}
+.underlineButton{
+  font-size: 1.3rem;
 }
 </style>
